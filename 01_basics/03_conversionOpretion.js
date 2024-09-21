29bcbdc6e344a565ecd5b4fd55320fbedce70062
Yashawant <yashwant@wcdevelopers.com>
2024-09-21 10:31:22 +0530

    convert variable

---
+++ 01_basics/03_conversionOpretion.js
@@ -0,0 +1,41 @@
+// let score = "33bfws";
+// let score = 33
+// let score = null;
+// let score = undefined;
+// let score = true
+// let score = false
+let score = "yashawant"
+console.log(typeof score);
+console.log(typeof (score));
+
+
+let valueInNumber = Number(score)
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
+
+
+/*
+number is easly convert "33" => 33
+number and string not a convert "33abcd" = NaN (Not A Number) typeof = number
+*/
+
+// let loggedIn = 1;
+// let loggedIn = ""
+let loggedIn = "yash"
+let booleanInLoggedIn = Boolean(loggedIn);
+console.log(booleanInLoggedIn);
+
+/*
+1 =>true; 0 => false
+"" => false 
+"yash" => true
+*/
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
