dd0e5bb696b332d606c35026d4b5ed6835c21da2
Yashawant <yashwant@wcdevelopers.com>
2024-09-21 09:52:05 +0530

    dataType done

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,27 @@
+ /* we are using node js not a browser*/
+//alert(3+3)
+
+let name = "yash";
+let age = 20;
+let state;
+
+/*
+Data types 
+
+1. number => 2 to power 53
+2. bigint
+3. string => ""
+4. boolean => true/false
+5. null => standalone value
+6. undefined
+7. symbol => unique
+
+object
+*/
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof state);
+
+
