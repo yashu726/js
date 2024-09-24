826e101187f7457c5a4bc055978ff90032269266
Yashawant <yashwant@wcdevelopers.com>
2024-09-25 00:04:23 +0530

    scopes is complete

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,23 @@
+// let a = 10 ;
+// const b= 20;
+// var c = 30;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+ var c = 300;  // var is not useable
+ let a = 100;  // globle scope
+if(true){    // block scope 
+    let a = 10 ;      
+const b= 20;
+var c = 30;
+console.log("INNER :", a);
+
+}
+console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+//  {}  this is scopes (yah kisi function ya if else ke sath aata hai hai to ese scopes bola jata hai)
