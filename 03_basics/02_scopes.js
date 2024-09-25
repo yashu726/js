d3563d3e109c41b9287c80ac434d2ee2885f21dc
Yashawant <yashwant@wcdevelopers.com>
2024-09-25 09:34:48 +0530

    Arrow function

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -6,6 +6,9 @@
 // console.log(b);
 // console.log(c);
 
+
+//  {}  this is scopes (yah kisi function ya if else ke sath aata hai hai to ese scopes bola jata hai)
+
  var c = 300;  // var is not useable
  let a = 100;  // globle scope
 if(true){    // block scope 
@@ -20,4 +23,51 @@ console.log(a);
 // console.log(c);
 
 
-//  {}  this is scopes (yah kisi function ya if else ke sath aata hai hai to ese scopes bola jata hai)
+
+/* +++++++++++++++++++++++++++++++++++ Neasted Scope  ++++++++++++++++++++++++++++++++ */
+
+function one (){
+    const username = "yashawant";
+
+    function two(){
+        const website = "yash.com";
+
+        console.log(username);
+        console.log(website);
+    }
+     // console.log(website); Error
+    two()
+}
+one()
+
+if (true) {
+    const username = "yashu";
+
+    if (username==="yashu") {
+        const website = "yashu.com";
+
+        console.log(username + website)
+        
+    }
+    // console.log(website);  Error
+    
+}
+
+// console.log(username);   Error
+
+
+/* +++++++++++++++++++++++++ Intresting +++++++++++++++++++++++++++++++++ */
+
+
+console.log(addOne(5));   // execute
+
+
+function addOne(num){
+    return num + 1;
+}
+
+addTwo(5)                      //  not execute
+const addTwo = function(num) {
+    return num + 2;
+}
+
---
+++ 03_basics/03_Arrow.js
@@ -0,0 +1,66 @@
+//  this means current context  refer krta hai  (current value ko use me leta hai)   context means value
+
+//  browser ke andar jo object hai wo window object hai
+
+const user = {
+    username : "yash",
+    price : 190,
+
+    welcomeMassege : function(){
+        console.log(`${this.username} welcome to website`);
+        // console.log(this);
+        
+        
+    }
+}
+
+// user.welcomeMassege();
+
+// user.username = "sam";
+
+// user.welcomeMassege()
+// console.log(this);
+
+
+// function one() {
+//     let username = "yash" 
+//     console.log(this.username);  // this object context ke liye hi work kr rha hai ese function ke liye work ni kr rha hai
+    
+// }
+
+// one()
+
+// const one = function() {
+//     let username = "yash" 
+//     console.log(this.username);  // this object context ke liye hi work kr rha hai ese function ke liye work ni kr rha hai
+    
+// }
+
+// one()
+
+const one = () => {                 // Arrow Function
+    let username = "yash" 
+    console.log(this.username);  
+    
+}
+
+// one()
+
+// basic arrow function
+
+const addTwo = (num1,num2) =>{
+    return num1 + num2;
+}
+
+// console.log(addTwo(2,3));
+
+//  implicit return arrow function
+
+// const addThree = (num1,num2) => num1 + num2;
+// const addThree = (num1,num2) => (num1 + num2);
+ const addThree = (num1,num2) => ({username : "yash"});   // object return
+
+
+console.log(addThree(2,3));
+
+
